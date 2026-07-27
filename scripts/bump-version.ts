eacebc93fd38083913c17b56510a8a3e7b1fbefd
Zablon Dawit <zabjd22@gmail.com>
2026-07-27 13:19:22 +0300

    feat: refactor bump-version to use ts-pattern
    - robust version bump cli :)
    - for authoritative place for storing scripts

--- scripts/bump-version.ts
+++ scripts/bump-version.ts
@@ -4,6 +4,8 @@ import { parseArgs, type ParseArgsConfig } from "node:util";
 import { log } from "node:console";
 import dedent from "dedent";
 import { z } from "zod";
+import { match, P } from "ts-pattern";
+import { tryCatch } from "../src/types/result.ts";
 
 const opts = {
   options: {
@@ -16,6 +18,7 @@ const opts = {
       short: "h",
     },
   },
+  allowPositionals: true,
 } as const as ParseArgsConfig;
 
 let $ = zx$({
@@ -35,23 +38,64 @@ function printUsage() {
   log(helpMessage);
 }
 
-function main() {
-  const { values: args } = parseArgs(opts);
-  const help = args.help;
-  if (help) {
-    printUsage();
-    return;
-  }
+type BumpType = z.infer<typeof bumpTypes>;
+const isBumpType = (version: string): version is BumpType =>
+  bumpTypes.safeParse(version).success;
 
-  const version = args.version;
-  const { success, data } = bumpTypes.safeParse(version);
-  if (!success) {
-    printUsage();
-    return;
-  }
-
-  $`echo "bumping ${data} version"`;
+function bumpVersion(version: BumpType) {
   // $`p changeset add --${version}`;
+  $`echo "not implemented"`;
+}
+
+function main() {
+  const result = tryCatch(() => parseArgs(opts), "failed to parse args");
+
+  match(result)
+    .when(
+      (result) => result.success,
+      ({ data: args }) => {
+        match(args)
+          .with(
+            {
+              values: {
+                help: true,
+              },
+            },
+            printUsage,
+          )
+          .with(
+            {
+              values: { version: P.string.and(P.when(isBumpType)).select() },
+            },
+            (version) => bumpVersion(version),
+          )
+          .with(
+            {
+              positionals: P.when(
+                (positionals) => P.array(P.string) && positionals.length > 1,
+              ),
+            },
+            ({ positionals }) => {
+              const version = positionals[1];
+              if (!isBumpType(version)) {
+                log("Invalid version specified:", version);
+                printUsage();
+                return;
+              }
+
+              bumpVersion(version);
+            },
+          )
+          .otherwise(() => {
+            log("No valid version specified");
+            printUsage();
+          });
+      },
+    )
+    .otherwise(({ message }) => {
+      log("Encountered Error:", message);
+      printUsage();
+    });
 }
 
 main();
