422c5763f4599fbee6fff73aae51b3f40670f408
Zablon Dawit <zabjd22@gmail.com>
2026-07-31 17:10:07 +0300

    chore: add build compression step in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import compression from "vite-plugin-compression2";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), compression()],
   resolve: {
     alias: {
-      '@': '/src',
-    }
-  }
-})
+      "@": "/src",
+    },
+  },
+});
