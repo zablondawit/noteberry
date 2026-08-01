91ad32a640badd1b41cde65aacfe87dc9a397ee9
Zablon Dawit <zabjd22@gmail.com>
2026-08-01 19:34:15 +0300

    feat: add vanilla-css for styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import compression from "vite-plugin-compression2";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), compression()],
+  plugins: [react(), compression(), vanillaExtractPlugin()],
   resolve: {
     alias: {
       "@": "/src",
