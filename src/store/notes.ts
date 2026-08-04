f7c315964be076759168cd5e0a19babd22334ff3
Zablon Dawit <zabjd22@gmail.com>
2026-08-04 10:17:54 +0300

    feat: add note & temporary note repositories

--- src/store/notes.ts
+++ src/store/notes.ts
@@ -1,32 +1,36 @@
-import { Dexie, type EntityTable } from "dexie";
-import { z } from "zod";
+import type { AsyncResult, Result } from "@/types/result";
+import type { DBInstance, NewNote, Note } from "./db";
 
-const baseNoteSchema = z.object({
-  id: z.number(),
-  title: z.string(),
-  content: z.string(),
-  tags: z.array(z.string()),
-  // convert to number
-  updatedAt: z.date().transform((date) => date.getTime()),
-});
-const noteSchema = baseNoteSchema.extend({});
-const tempNoteSchema = baseNoteSchema
-  .pick({
-    id: true,
-    updatedAt: true,
-    content: true,
-  })
-  .extend({});
+type FindBy = keyof Pick<Note, "id">;
 
-export type Note = z.infer<typeof noteSchema>;
-export type TempNote = z.infer<typeof tempNoteSchema>;
+export interface NoteRepository {
+  find<T extends FindBy>(by: T, value: Note[T]): AsyncResult<Note>;
+  add(note: NewNote): AsyncResult<Note>;
+  update(id: number, note: NewNote): AsyncResult<Note>;
+  delete(id: number): AsyncResult<Note["id"]>;
+}
 
-export const db = new Dexie("noteberry_notes") as Dexie & {
-  notes: EntityTable<Note>;
-  tempNotes: EntityTable<TempNote>;
+type NoteRepositoryContext = {
+  db: DBInstance;
 };
 
-db.version(1).stores({
-  notes: "++id,title,content,tags,updatedAt",
-  tempNotes: "++id,content,updatedAt",
-});
+export class NoteRepositoryLive implements NoteRepository {
+  ctx: NoteRepositoryContext;
+
+  constructor(ctx: NoteRepositoryContext) {
+    this.ctx = ctx;
+  }
+
+  find<T extends FindBy>(by: T, value: Note[T]): AsyncResult<Note> {
+    throw new Error("Method not implemented.");
+  }
+  add(note: NewNote): AsyncResult<Note> {
+    throw new Error("Method not implemented.");
+  }
+  update(id: number, note: Partial<NewNote>): AsyncResult<Note> {
+    throw new Error("Method not implemented.");
+  }
+  delete(id: number): AsyncResult<NonNullable<Note["id"]>> {
+    throw new Error("Method not implemented.");
+  }
+}
