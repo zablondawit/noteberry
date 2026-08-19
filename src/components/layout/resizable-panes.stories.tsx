a827dd974555baebd654cf7243d0a4eb14b61edd
Zablon Dawit <zabjd22@gmail.com>
2026-08-19 13:25:52 +0300

    chore: update resizable-panes.stories.tsx

--- src/components/layout/resizable-panes.stories.tsx
+++ src/components/layout/resizable-panes.stories.tsx
@@ -12,10 +12,14 @@ export default meta;
 
 type Story = StoryObj<typeof ResizableContainer>;
 
+const DummyContent = ({ text }: { text: string }) => (
+  <span className="w-full h-full flex items-center justify-center">{text}</span>
+);
+
 export const DefaultLayout: Story = {
   args: {
-    leftPane: <span>Left</span>,
-    rightPane: <span>Right</span>,
+    leftPane: <DummyContent text="Left Pane" />,
+    rightPane: <DummyContent text="Right Pane" />,
   },
   render: (props) => (
     <div className="h-screen w-screen">
